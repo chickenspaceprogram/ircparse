f4e766569bff931d5fb828432098cf23d24271b2
Athena Boose <pestpestthechicken@yahoo.com>
2024-12-14 00:26:05 -0800

    Added arraylist lib.

--- include/ircparse/ircparse.h
+++ include/ircparse/ircparse.h
@@ -6,13 +6,27 @@
 
 #ifndef IRCPARSE_H
 #define IRCPARSE_H
+#include <stddef.h>
 
-typedef struct irc_prefix {
-    int yeet;
-} IrcPrefix;
+typedef struct irc_tag {
+    char *key;
+    char *value;
+} IrcTag;
 
 typedef struct irc_msg {
-    int yeet;
+    IrcTag *tags;
+    size_t num_tags;
+    char *server_name;
+    char nick[10]; // nick is 10 long because it's convenient for it to also be a C-string
+    char *user;
+    char *host;
+    char *command;
+    char **params;
+    size_t num_params;
 } IrcMsg;
+
+IrcMsg initIrcMsg(void);
+
+IrcMsg freeIrcMsg(void);
 
 #endif
--- src/ircparse/ircparse.c
+++ src/ircparse/ircparse.c
@@ -6,3 +6,19 @@
 
 #include <ircparse/ircparse.h>
 
+IrcMsg initIrcMsg(void) {
+    IrcMsg msg = {
+        .tags = NULL,
+        .num_tags = 0,
+        .server_name = NULL,
+        .nick = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'},
+        .user = NULL,
+        .host = NULL,
+        .command = NULL,
+        .params = NULL,
+        .num_params = 0,
+    }
+
+}
+
+IrcMsg freeIrcMsg(void);
